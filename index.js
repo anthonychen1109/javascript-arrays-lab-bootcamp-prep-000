a1fafd7ae7dd753784100560fc47c5af85bd4e6e
Anthony Chen <chen.anthony.1109@gmail.com>
2018-06-04 03:40:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const app = "I don't do much."
 function destructivelyAppendKitten(name) {
   kittens.push(name)
 }
+
+function destructivelyPrependKitten(name) {
+  kitten.unshift(name)
+  return kitten
+}
+
+function 
